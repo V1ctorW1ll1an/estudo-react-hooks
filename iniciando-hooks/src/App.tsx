2a8bee8c3811e5b14922e9619ff7b1f8ffced731
Victor Willian de Sousa <victorwillian1012@gmail.com>
2022-02-01 20:29:42 -0300

    useMemo

--- iniciando-hooks/src/App.tsx
+++ iniciando-hooks/src/App.tsx
@@ -1,26 +1,23 @@
-import { useCallback, useState } from "react";
+import { ChangeEvent, useMemo, useState } from "react";
 import "./App.css";
-import { Button } from "./components/Button";
-
-//https://pt-br.reactjs.org/docs/hooks-rules.html
+import { IPosts, Post } from "./components/Post";
 
 function App() {
-    console.log("pai render");
-    const [counter, setCounter] = useState(0);
-
-    // o useCallback deve ser usado quando temos uma função pesada
-    // que não precisa ser renderizada novamente
-
-    const updateCounter = useCallback(
-        (value: number): void =>
-            setCounter((prevCounter) => prevCounter + value),
-        []
-    );
-
+    const [search, setSearch] = useState("");
     return (
         <div>
-            <h1>contador: {counter}</h1>
-            <Button updateCounter={updateCounter} />
+            <div>
+                <input
+                    type="search"
+                    value={search}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                        setSearch(e.target.value)
+                    }
+                />
+            </div>
+            {useMemo(() => {
+                return <Post />;
+            }, [])}
         </div>
     );
 }
---
+++ iniciando-hooks/src/components/Post.tsx
@@ -0,0 +1,38 @@
+import _ from "lodash";
+import { useEffect, useState } from "react";
+
+export interface IPosts {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+}
+
+const Post = () => {
+    const [posts, setPosts] = useState<Array<IPosts>>([]);
+
+    const getPosts = async (): Promise<void> => {
+        await fetch("https://jsonplaceholder.typicode.com/posts")
+            .then((response: Response) => response.json())
+            .then((response: Array<IPosts>) => setPosts(response))
+            .catch((err) => console.log(err));
+    };
+
+    useEffect(() => {
+        getPosts();
+    }, []);
+    return (
+        <ul>
+            {_.map(posts, (post: IPosts) => {
+                return (
+                    <li className="post" key={post.id}>
+                        <h1 className="post-title">{post.title}</h1>
+                        <p className="post-body">{post.body}</p>
+                    </li>
+                );
+            })}
+        </ul>
+    );
+};
+
+export { Post };
