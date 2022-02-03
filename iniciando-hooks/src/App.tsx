b2e351220013432d1f3388123e0e2bf512036efa
Victor Willian de Sousa <victorwillian1012@gmail.com>
2022-02-02 21:34:12 -0300

    useRef

--- iniciando-hooks/src/App.tsx
+++ iniciando-hooks/src/App.tsx
@@ -1,15 +1,23 @@
-import { ChangeEvent, useMemo, useState } from "react";
+import { ChangeEvent, useEffect, useMemo, useRef, useState } from "react";
 import "./App.css";
 import { IPosts, Post } from "./components/Post";
 
 function App() {
     const [search, setSearch] = useState("");
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        inputRef.current?.focus();
+        console.log(inputRef.current);
+    }, [search]);
+
     return (
         <div>
             <div>
                 <input
                     type="search"
                     value={search}
+                    ref={inputRef}
                     onChange={(e: ChangeEvent<HTMLInputElement>) =>
                         setSearch(e.target.value)
                     }
